d46d512f3bb987311c5e830de65807d1186af01a
Magnus Manske <magnusmanske@googlemail.com>
2023-11-22 11:33:40 +0000

    misc

--- test.sh
+++ test.sh
@@ -24,7 +24,7 @@ md5_expected=$(cat "${full_archive}" | md5)
 echo "STARTING"
 # 512513
 # 811520
-for ((first=811518; first<=max_position; first++)); do
+for ((first=0; first<=max_position; first++)); do
     after=$((first + 1))
     md5sum=$( ( target/release/tarlive --input test_data/test.files | head -c $first ; target/release/tarlive --input test_data/test.files --offset $after ) | md5 )
     if [ "${md5_expected}" != "${md5sum}" ]
